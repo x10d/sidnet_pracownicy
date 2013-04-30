e88acedd94be2763c7ab4f669f1ead6d971364cc
x10d <x10d@djek.toya.net.pl>
2013-04-30 09:53:19 +0200

    Kosmetyka pliku views/masonryPix.php: - dodanie ID do diva obrazka - usuniecie zakomentowanego niepotrzebnego die(print_r());

--- application/views/masonryPix.php
+++ application/views/masonryPix.php
@@ -1,10 +1,9 @@
 <p>Masonry - pix</p>
 
 <div id="masonryItemContainer">
-    <?php //die(print_r($pixTable)) ?>
     <?php foreach ($pixTable as $key => $value): ?>
     <div class="masonryItem">
-        <img id="<?php echo $key ?>" src="<?php echo $value ?>"/>
+        <img id="mItem<?php echo $key ?>" src="<?php echo $value ?>"/>
     </div>
     <?php endforeach; ?>
 </div>
