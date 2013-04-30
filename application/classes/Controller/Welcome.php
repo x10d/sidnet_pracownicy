b3560ed8ff77861b678265329a2a88a334e1fe8a
x10d <x10d@djek.toya.net.pl>
2013-04-30 13:47:43 +0200

    Dodany infinite scroll: - dodana biblioteka jquery.infinitescroll.js - dodana obsluga powyzszej biblioteki - dodane doklejanie zdjec z kolejnych podstron - zablokowane wyswietlanie z nieistniejacych podstron (wyzszych niz $pagination->total_pages)

--- application/classes/Controller/Welcome.php
+++ application/classes/Controller/Welcome.php
@@ -17,9 +17,24 @@ class Controller_Welcome extends Controller_Base
 
         $pixTable = $modelPictures->getList($pagination);
         $pageLinks = $pagination->render();
-        $this->template->content = View::factory('masonryPix')
-            ->set('pixTable', $pixTable)
-            ->bind('pageLinks', $pageLinks);
+
+        if ($pagination->total_pages < $this->request->query('page')) {
+            die;
+        }
+
+        $res = '';
+        if ($this->request->is_ajax()) {
+                foreach ($pixTable as $key => $value) {
+                $res .= '<div class="masonryItem"><img class="imagediv" id="mItem' . $key . '" src="' . $value . '"/></div>';
+            }
+            $response = $res . $pageLinks;
+            $this->request->headers('Content-type', 'application/json; charset=' . Kohana::$charset);
+            $this->response->body($res);
+        } else {
+            $this->template->content = View::factory('masonryPix')
+                ->set('pixTable', $pixTable)
+                ->bind('pageLinks', $pageLinks);
+        }
     }
 
 } // End Welcome
--- application/views/masonryPix.php
+++ application/views/masonryPix.php
@@ -1,22 +1,46 @@
 <p>Masonry - pix</p>
 
-<?php echo $pageLinks ?>
 <div id="masonryItemContainer">
     <?php foreach ($pixTable as $key => $value): ?>
     <div class="masonryItem">
-        <img id="mItem<?php echo $key ?>" src="<?php echo $value ?>"/>
+        <img class="imagediv" id="mItem<?php echo $key ?>" src="<?php echo $value ?>"/>
     </div>
     <?php endforeach; ?>
 </div>
 <?php echo $pageLinks ?>
-<script type="text/javascript">
-    $(document).ready(function(){
-        $(function(){
-            $('#masonryItemContainer').masonry({
-                // options
-                itemSelector : '.masonryItem',
-                columnWidth : 240
-            });
+
+<script>
+    $(function(){
+
+        var $container = $('#masonryItemContainer');
+
+        $container.imagesLoaded(function(){
+          $container.masonry({
+            itemSelector: '.masonryItem',
+            columnWidth: 240
+          });
         });
+
+        $container.infinitescroll({
+          navSelector  : '.pagination',    // selector for the paged navigation 
+          nextSelector : '.pagination a[rel="next"]',  // selector for the NEXT link (to page 2)
+          itemSelector : '.masonryItem',     // selector for all items you'll retrieve
+          loading: {
+              finishedMsg: 'No more pages to load.',
+              img: 'http://i.imgur.com/6RMhx.gif'
+            }
+          },
+          // trigger Masonry as a callback
+          function( newElements ) {
+            // hide new items while they are loading
+            var $newElems = $( newElements ).css({ opacity: 0 });
+            // ensure that images load before adding to masonry layout
+            $newElems.imagesLoaded(function(){
+              // show elems now they're ready
+              $newElems.animate({ opacity: 1 });
+              $container.masonry( 'appended', $newElems, true ); 
+            });
+          }
+        );
     });
 </script>
